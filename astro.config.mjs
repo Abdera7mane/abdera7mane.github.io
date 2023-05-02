085be6913c2150c0f6d469d1a063ac21a69eb297
Abdera7mane <59313921+Abdera7mane@users.noreply.github.com>
2023-05-02 21:11:58 +0100

    Exclude `astrojs/image` from Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://abdera7mane.github.io/',
+  site: 'https://abdera7mane.github.io/'
 });
