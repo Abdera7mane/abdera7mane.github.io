857c89fd88f3d84353690efddea46e8a3b7afa3b
Abdera7mane <59313921+Abdera7mane@users.noreply.github.com>
2023-05-02 20:53:02 +0100

    It had to be more representable

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import image from "@astrojs/image";
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://abdera7mane.github.io'
-})
+  site: 'https://abdera7mane.github.io/',
+});
